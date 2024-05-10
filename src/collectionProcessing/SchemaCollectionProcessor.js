063847ab84a207f3d2bfebf5efa72c44c1103578
voidedWarranties <hi+github@voidedxd.xyz>
2024-05-10 11:18:54 -0700

    Search for schemas by glob

--- src/collectionProcessing/SchemaCollectionProcessor.js
+++ src/collectionProcessing/SchemaCollectionProcessor.js
@@ -1,5 +1,6 @@
 import fs from 'fs-extra'
 import path from 'path'
+import anymatch from 'anymatch'
 import { validate } from '@hyperjump/json-schema/draft-2020-12'
 import CollectionProcessor from './CollectionProcessor.js'
 import MarkdownFileProcessor from '../compile/markdown/MarkdownFileProcessor.js'
@@ -14,7 +15,13 @@ export default class SchemaCollectionProcessor extends CollectionProcessor {
       let msg
 
       if (vf.path.endsWith('.toml')) {
-        schemaPath = this.config.schemas[path.basename(vf.path)]
+        for (const [key, schema] of Object.entries(this.config.schemas)) {
+          if (!anymatch(key, vf.path)) { continue }
+
+          schemaPath = schema
+          break
+        }
+
         if (!schemaPath) { continue }
 
         obj = JSON.parse(await fs.readFile(path.join(outDir, TomlFileProcessor.getOutputPath(vf.path))))
@@ -30,7 +37,13 @@ export default class SchemaCollectionProcessor extends CollectionProcessor {
         continue
       }
 
-      const validateResult = await validate(path.join(inDir, schemaPath), obj)
+      const fullSchemaPath = path.join(inDir, schemaPath)
+      if (!await fs.exists(fullSchemaPath)) {
+        vfileMessage(vf, null, 'schema-not-found', `Schema file ${schemaPath} does not exist`)
+        continue
+      }
+
+      const validateResult = await validate(fullSchemaPath, obj)
 
       if (!validateResult.valid) {
         vfileMessage(vf, null, 'schema-validate', msg)

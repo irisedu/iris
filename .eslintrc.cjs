99927fdb8ed10c27e631c1c54793aee081770abb
voidedWarranties <hi+github@voidedxd.xyz>
2024-06-02 21:49:15 -0700

    Add .prettierignore

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,5 @@
 module.exports = {
 	env: { node: true, es2020: true },
 	extends: ['eslint:recommended', 'prettier'],
-    parserOptions: { ecmaVersion: 'latest', sourceType: 'module' }
+	parserOptions: { ecmaVersion: 'latest', sourceType: 'module' }
 };

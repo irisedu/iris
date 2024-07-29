4522c587dd262124c727b5fcd367e0311a19c035
voidedWarranties <hi+github@voidedxd.xyz>
2024-07-29 16:53:38 -0700

    Update styles and add shared aria components

---
+++ src/components/aria/menu.jsx
@@ -0,0 +1,23 @@
+import { MenuItem, SubmenuTrigger, Popover, Menu } from 'react-aria-components';
+
+import ChevronRight from '~icons/tabler/chevron-right';
+
+export function SubmenuItem({ children }) {
+	return (
+		<MenuItem className="react-aria-MenuItem flex flex-row items-center">
+			{children}
+			<ChevronRight className="w-4 h-4 ml-auto" />
+		</MenuItem>
+	);
+}
+
+export function Submenu({ children, label }) {
+	return (
+		<SubmenuTrigger>
+			<SubmenuItem>{label}</SubmenuItem>
+			<Popover>
+				<Menu aria-label={label}>{children}</Menu>
+			</Popover>
+		</SubmenuTrigger>
+	);
+}

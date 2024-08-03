ea4606586392e81fef3173847844db9e0f116601
voidedWarranties <hi+github@voidedxd.xyz>
2024-08-03 11:55:44 -0700

    Update packages

---
+++ eslint.config.mjs
@@ -0,0 +1,59 @@
+import js from '@eslint/js';
+import prettier from 'eslint-config-prettier';
+import react from 'eslint-plugin-react';
+
+import { fixupConfigRules } from '@eslint/compat';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import globals from 'globals';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { FlatCompat } from '@eslint/eslintrc';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+	baseDirectory: __dirname,
+	recommendedConfig: js.configs.recommended,
+	allConfig: js.configs.all
+});
+
+export default [
+	js.configs.recommended,
+	prettier,
+	react.configs.flat.recommended,
+	react.configs.flat['jsx-runtime'],
+	...fixupConfigRules(compat.extends('plugin:react-hooks/recommended')),
+	{
+		files: ['**/*.js', '**/*.jsx']
+	},
+	{
+		ignores: ['**/dist/*']
+	},
+	{
+		plugins: {
+			'react-refresh': reactRefresh
+		},
+
+		languageOptions: {
+			globals: {
+				...globals.browser
+			},
+
+			ecmaVersion: 'latest',
+			sourceType: 'module'
+		},
+
+		settings: {
+			react: {
+				version: '18.2'
+			}
+		},
+
+		rules: {
+			'react/jsx-no-target-blank': 'off',
+			'react/prop-types': 'off',
+			'react-refresh/only-export-components': 'off',
+			'import/no-absolute-path': 'off'
+		}
+	}
+];

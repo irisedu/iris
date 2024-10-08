50edb30afaa78d89b3ec1204953770c0fccb0633
voidedWarranties <hi+github@voidedxd.xyz>
2024-10-08 01:54:01 -0700

    frontend: placeholder landing page v2

--- packages/iris-frontend/src/routes/Landing.tsx
+++ packages/iris-frontend/src/routes/Landing.tsx
@@ -7,23 +7,11 @@ export function Component() {
 
 	return (
 		<>
-			<h1>Heading 1</h1>
-			<p>Prose text</p>
-
-			<h2>Heading 2</h2>
-			<p>Prose text</p>
-
-			<h3>Heading 3</h3>
-			<p>Prose text</p>
-
-			<h4>Heading 4</h4>
-			<p>Prose text</p>
-
-			<h5>Heading 5</h5>
-			<p>Prose text</p>
-
-			<h6>Heading 6</h6>
-			<p>Prose text</p>
+			<h1>Welcome to Iris!</h1>
+			<p>
+				Iris is your no-nonsense, open-source content management system built
+				for education.
+			</p>
 		</>
 	);
 }

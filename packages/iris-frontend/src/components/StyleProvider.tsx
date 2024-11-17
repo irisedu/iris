be0ab8263b7021bec468e6acc8d2bd9e2341b4ef
voidedWarranties <hi+github@voidedxd.xyz>
2024-11-16 22:46:36 -0800

    frontend: set font choice for whole page

--- packages/iris-frontend/src/components/StyleProvider.tsx
+++ packages/iris-frontend/src/components/StyleProvider.tsx
@@ -30,40 +30,32 @@ function StyleProvider({
 	children: ReactNode;
 	className: string;
 }) {
-	const font = useSelector((state: RootState) => state.prefs.text.font);
-	const fontSize = useSelector((state: RootState) => state.prefs.text.fontSize);
-
-	const charSpacing = useSelector(
-		(state: RootState) => state.prefs.text.charSpacing
-	);
-	const wordSpacing = useSelector(
-		(state: RootState) => state.prefs.text.wordSpacing
-	);
-	const lineSpacing = useSelector(
-		(state: RootState) => state.prefs.text.lineSpacing
-	);
-
+	const textSettings = useSelector((state: RootState) => state.prefs.text);
 	const hueShift = useSelector((state: RootState) => state.prefs.hueShift);
 
-	useEffect(() => {
-		document.documentElement.style.fontSize = fontSize + '%';
-	}, [fontSize]);
-
-	useEffect(() => {
-		document.documentElement.style.setProperty(
-			'--hue-shift',
-			hueShift.toString()
-		);
-	}, [hueShift]);
+	const { font, fontSize, charSpacing, wordSpacing, lineSpacing } =
+		textSettings;
 
 	const settings = fontSettings[font];
 
-	const style = {
-		'--font-body': font,
-		'--font-smallcaps': settings?.smallcaps ?? font,
-		'--font-sans': settings?.sans ?? font,
-		fontSynthesis: settings?.fontSynthesis ?? 'none',
+	useEffect(() => {
+		const style = document.documentElement.style;
 
+		style.fontSize = fontSize + '%';
+		style.setProperty('--font-body', font);
+		style.setProperty('--font-smallcaps', settings?.smallcaps ?? font);
+		style.setProperty('--font-sans', settings?.sans ?? font);
+		style.fontSynthesis = settings?.fontSynthesis ?? 'none';
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [textSettings]);
+
+	useEffect(() => {
+		const style = document.documentElement.style;
+
+		style.setProperty('--hue-shift', hueShift.toString());
+	}, [hueShift]);
+
+	const style = {
 		letterSpacing: charSpacing + 'em',
 		wordSpacing: wordSpacing < 0 ? 'normal' : wordSpacing + 'em',
 		lineHeight: lineSpacing.toString()

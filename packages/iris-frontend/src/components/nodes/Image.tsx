36f90f083eb786b1da219ca1c7e25111fbf12b09
Wong Zhao <193191274+wongzhao@users.noreply.github.com>
2025-03-04 14:35:17 -0800

    [frontend] click to expand images

--- packages/iris-frontend/src/components/nodes/Image.tsx
+++ packages/iris-frontend/src/components/nodes/Image.tsx
@@ -1,7 +1,16 @@
 import { useSelector } from 'react-redux';
 import { type RootState } from '$state/store';
+import { Button, Dialog, DialogTrigger, Modal } from 'iris-components';
 
-function Image({ src, alt }: { src: string; alt: string }) {
+import X from '~icons/tabler/x';
+
+export interface ImageProps {
+	src: string;
+	alt: string;
+	className?: string;
+}
+
+function ImageInternal({ src, alt, className }: ImageProps) {
 	const devEnabled = useSelector((state: RootState) => state.dev.enabled);
 	const devHost = useSelector((state: RootState) => state.dev.host);
 
@@ -12,8 +21,18 @@ function Image({ src, alt }: { src: string; alt: string }) {
 	if (devEnabled && src.startsWith('/')) {
 		if (isSvg) {
 			return (
-				<object type="image/svg+xml" data={devSrc} aria-label={alt}>
-					<object type="image/svg+xml" data={src} aria-label={alt}>
+				<object
+					type="image/svg+xml"
+					data={devSrc}
+					aria-label={alt}
+					className={'pointer-events-none' + (className ? ' ' + className : '')}
+				>
+					<object
+						type="image/svg+xml"
+						data={src}
+						aria-label={alt}
+						className={className}
+					>
 						{alt}
 					</object>
 				</object>
@@ -22,21 +41,52 @@ function Image({ src, alt }: { src: string; alt: string }) {
 			return (
 				<picture>
 					<source srcSet={devSrc} />
-					<img src={src} alt={alt} />
+					<img src={src} alt={alt} className={className} />
 				</picture>
 			);
 		}
 	} else {
 		if (isSvg) {
 			return (
-				<object type="image/svg+xml" data={src} aria-label={alt}>
+				<object
+					type="image/svg+xml"
+					data={src}
+					aria-label={alt}
+					className={'pointer-events-none' + (className ? ' ' + className : '')}
+				>
 					{alt}
 				</object>
 			);
 		} else {
-			return <img src={src} alt={alt} loading="lazy" />;
+			return <img src={src} alt={alt} loading="lazy" className={className} />;
 		}
 	}
+}
+
+function Image(props: ImageProps) {
+	return (
+		<DialogTrigger>
+			<Button className="cursor-pointer w-full">
+				<ImageInternal {...props} />
+			</Button>
+			{/* Sizing nightmare: Try to maximize image size based on height */}
+			<Modal isDismissable className="h-full">
+				<Dialog className="react-aria-Dialog h-full flex flex-col items-center">
+					<Button
+						className="fixed top-5 right-5 rounded-full text-black bg-iris-100 data-[hovered]:bg-iris-200 data-[pressed]:bg-iris-300 p-1 cursor-pointer"
+						aria-label="Close image popup"
+						slot="close"
+					>
+						<X />
+					</Button>
+					<div className="grow shadow-lg h-full">
+						<ImageInternal {...props} className="size-full dark:bg-black" />
+					</div>
+					{props.alt && <p className="text-sm text-center my-3">{props.alt}</p>}
+				</Dialog>
+			</Modal>
+		</DialogTrigger>
+	);
 }
 
 export default Image;
